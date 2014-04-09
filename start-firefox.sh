414cf448dcaa469b3d10151d00efff7d17a65b92
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-09 13:24:23 +1000

    Manually initialize firefox profile

--- start-firefox.sh
+++ start-firefox.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-firefox $@
+FIREFOX_HOME=$HOME/.mozilla/firefox
+PROFILE_DIR=$FIREFOX_HOME/test.profile
+
+rm -rf $PROFILE_DIR
+mkdir -p $PROFILE_DIR
+cp ./firefox-prefs.js $PROFILE_DIR/prefs.js
+printf "{\n\"created\": $(date +%s%N | cut -b1-13)\n}" > $PROFILE_DIR/times.json
+printf "[Profile0]\nName=test\nIsRelative=0\nPath=test.profile\nDefault=1" > $FIREFOX_HOME/profiles.ini
+# touch $PROFILE_DIR/.parentlock
+
+firefox -profile test $@
