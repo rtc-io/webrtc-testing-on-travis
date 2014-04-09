4395009308d551eddb4ec06b9fdd40ef49c2b0e0
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-09 12:52:15 +1000

    Added test

---
+++ test/capture-media.js
@@ -0,0 +1,29 @@
+var test = require('tape');
+var media = require('rtc-media');
+
+test('MediaStream constructor is defined', function(t) {
+  t.plan(1);
+  t.equal(typeof MediaStream, 'function', 'have MediaStream constructor');
+});
+
+test('can capture a media stream', function(t) {
+  var handleError = t.ifError.bind(t);
+  var localMedia;
+  var timer = setTimeout(t.fail.bind(t, 'timed out'), 5000);
+
+  t.plan(3);
+
+  // capture media
+  localMedia = media();
+  t.pass('invoked rtc-media capture function successfully');
+
+  localMedia.on('error', handleError);
+
+  localMedia.once('capture', function(stream) {
+    clearTimeout(timer);
+    localMedia.removeListener('error', handleError);
+
+    t.pass('captured stream');
+    t.ok(stream instanceof MediaStream, 'stream is valid');
+  });
+});
