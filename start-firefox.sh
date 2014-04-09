267c6a67923dd2a6914f4a43ea02c507242daa19
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-09 13:31:05 +1000

    Modified profile creation

--- start-firefox.sh
+++ start-firefox.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 FIREFOX_HOME=$HOME/.mozilla/firefox
-PROFILE_DIR=$FIREFOX_HOME/test.profile
+PROFILE_DIR=$FIREFOX_HOME/rtctest.profile
 
 rm -rf $PROFILE_DIR
 mkdir -p $PROFILE_DIR
 cp ./firefox-prefs.js $PROFILE_DIR/prefs.js
-printf "{\n\"created\": $(date +%s%N | cut -b1-13)\n}" > $PROFILE_DIR/times.json
-printf "[Profile0]\nName=test\nIsRelative=0\nPath=test.profile\nDefault=1" > $FIREFOX_HOME/profiles.ini
-# touch $PROFILE_DIR/.parentlock
+# printf "{\n\"created\": $(date +%s%N | cut -b1-13)\n}" > $PROFILE_DIR/times.json
 
-firefox -profile test $@
+firefox -profile $PROFILE_DIR $@
