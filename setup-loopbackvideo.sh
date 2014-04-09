6c5557c428c44b6379dd591069382884883ec77b
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-09 15:56:58 +1000

    Tweaks to setup script to attempt to install v4l2loopback-dkms from apt

--- setup-loopbackvideo.sh
+++ setup-loopbackvideo.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
-yes "\n" | sudo add-apt-repository -y ppa:gstreamer-developers/ppa
-sudo apt-get update --fix-missing
-sudo apt-get install user-mode-linux linux-headers-$(uname -r) build-essential gstreamer1.0-tools gstreamer1.0-libav gstreamer1.0-plugins-good libvpx1 libopus0
+sudo apt-get install -y v4l2loopback-dkms
 
-# compile the v4l loopback driver
-git clone https://github.com/umlaeute/v4l2loopback.git
-cd v4l2loopback
-make && sudo make install
+# yes "\n" | sudo add-apt-repository -y ppa:gstreamer-developers/ppa
+# sudo apt-get update --fix-missing
+# sudo apt-get install -y gstreamer1.0-tools gstreamer1.0-libav gstreamer1.0-plugins-good libvpx1 libopus0
 
-# install vido
-mkdir -p vido
-curl -s https://codeload.github.com/g2p/vido/tar.gz/v0.3.2 | tar -xz --strip-components=1 --directory vido
+# # compile the v4l loopback driver
+# git clone https://github.com/umlaeute/v4l2loopback.git
+# cd v4l2loopback
+# make && sudo make install
 
-# ensure the loopback video device is loaded
-sudo modprobe v4l2loopback
+# # install vido
+# mkdir -p vido
+# curl -s https://codeload.github.com/g2p/vido/tar.gz/v0.3.2 | tar -xz --strip-components=1 --directory vido
 
-# note new video device to use below
-ls -ld /dev/video*
+# # ensure the loopback video device is loaded
+# sudo modprobe v4l2loopback
 
-# create a fake video source
-gst-launch-1.0 videotestsrc pattern=smpte100 ! v4l2sink device=/dev/video0 &
+# # note new video device to use below
+# ls -ld /dev/video*
+
+# # create a fake video source
+# gst-launch-1.0 videotestsrc pattern=smpte100 ! v4l2sink device=/dev/video0 &
