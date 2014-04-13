6438e9372f60ba70bb317db7c80ae391ab83ac34
Damon Oehlman <damon.oehlman@gmail.com>
2014-04-13 20:44:03 +1000

    Tweak start-firefox script to create our own profile

--- start-firefox.sh
+++ start-firefox.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 FIREFOX_HOME=$HOME/.mozilla/firefox
-PROFILE_DIR=$FIREFOX_HOME/rtctest.profile
 
 if [ -f ./firefox-prefs.js ]; then
-  rm -rf $PROFILE_DIR
-  mkdir -p $PROFILE_DIR
-  cp ./firefox-prefs.js $PROFILE_DIR/prefs.js
+  rm -rf $FIREFOX_HOME/*.integration-test
+  firefox --no-remote -CreateProfile integration-test
+  cp ./firefox-prefs.js $FIREFOX_HOME/*.integration-test/prefs.js
 fi
 # printf "{\n\"created\": $(date +%s%N | cut -b1-13)\n}" > $PROFILE_DIR/times.json
 
-AUDIODEV=null firefox -profile $PROFILE_DIR $@
+firefox --no-remote -profile integration-test $@
