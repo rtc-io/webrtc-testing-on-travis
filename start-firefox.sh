a6c1db5d4f3508a3e98a789af593f483c803fe4a
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-14 12:49:35 +1000

    Reference the script path when looking for the preferences file

--- start-firefox.sh
+++ start-firefox.sh
@@ -1,10 +1,19 @@
 #!/usr/bin/env bash
 FIREFOX_HOME=$HOME/.mozilla/firefox
 
-if [ -f ./firefox-prefs.js ]; then
+# determine the script path
+# ref: http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
+pushd `dirname $0` > /dev/null
+SCRIPTPATH=`pwd -P`
+popd > /dev/null
+
+# set the prefs location
+PREFS=$SCRIPTPATH/firefox-prefs.js
+
+if [ -f $PREFS ]; then
   rm -rf $FIREFOX_HOME/*.integrationtest
   firefox --no-remote -CreateProfile integrationtest
-  cp ./firefox-prefs.js $FIREFOX_HOME/*.integrationtest/prefs.js
+  cp $SCRIPTPATH/firefox-prefs.js $FIREFOX_HOME/*.integrationtest/prefs.js
 fi
 # printf "{\n\"created\": $(date +%s%N | cut -b1-13)\n}" > $PROFILE_DIR/times.json
 
