ccdb2b5ebc3c39de93a6c0aec25dcb745c12eeec
Damon Oehlman <damon.oehlman@gmail.com>
2014-08-14 15:43:04 +1000

    Allow customization of the command used to start firefox

--- start-firefox.sh
+++ start-firefox.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-FIREFOX_HOME=$HOME/.mozilla/firefox
+FIREFOX_COMMAND=${FIREFOX_COMMAND-firefox}
+FIREFOX_HOME=$HOME/.mozilla/$FIREFOX_COMMAND
 
 # determine the script path
 # ref: http://stackoverflow.com/questions/4774054/reliable-way-for-a-bash-script-to-get-the-full-path-to-itself
@@ -12,9 +13,9 @@ PREFS=$SCRIPTPATH/firefox-prefs.js
 
 if [ -f $PREFS ]; then
   rm -rf $FIREFOX_HOME/*.integrationtest
-  firefox --no-remote -CreateProfile integrationtest
+  $FIREFOX_COMMAND --no-remote -CreateProfile integrationtest
   cp $SCRIPTPATH/firefox-prefs.js $FIREFOX_HOME/*.integrationtest/prefs.js
 fi
 # printf "{\n\"created\": $(date +%s%N | cut -b1-13)\n}" > $PROFILE_DIR/times.json
 
-firefox --no-remote -P integrationtest $@
+$FIREFOX_COMMAND --no-remote -P integrationtest $@
