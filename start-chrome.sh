e2de572253156d6df0be99ff530971b3ad311ac9
Damon Oehlman <damon.oehlman@gmail.com>
2014-10-09 22:20:47 +1100

    Suppress chrome error reporting as it is clogging up travis log output

--- start-chrome.sh
+++ start-chrome.sh
@@ -5,6 +5,7 @@ EXTRA_OPTS="--no-sandbox --disable-setuid-sandbox --allow-sandbox-debugging"
 google-chrome \
   --console \
   --no-first-run \
+  --log-level=3 \
   --use-fake-device-for-media-stream \
   --use-fake-ui-for-media-stream $@ \
   --user-data-dir
